181f55cd417e54017f7c157edccde574b7f00c32
Sli <antoine@bartuccio.fr>
2025-12-26 23:18:46 +0100

    Remove everything static, introduce css file and make left layout work

--- NeoCodeMap.py
+++ NeoCodeMap.py
@@ -1,13 +1,18 @@
-from typing import Union, List, Optional, Dict
+from __future__ import annotations
+from typing import Union, List, Optional, Dict, Literal
 import sublime
 import sublime_plugin
 
+map_manager: CodeMapManager = None
 settings: sublime.Settings = None
+css: str = ""
 
 
 def plugin_loaded():
-    global settings
+    global settings, css, map_manager
+    map_manager = CodeMapManager()
     settings = sublime.load_settings("NeoCodeMap.sublime-settings")
+    css = sublime.load_resource("Packages/NeoCodeMap/NeoCodeMap.css")
 
 
 def plugin_unloaded():
@@ -15,8 +20,6 @@ def plugin_unloaded():
 
 
 class CodeMapManager:
-    _html_sheets: List[int] = []
-
     KIND_CLASS_NAMES: Dict[int, str] = {
         sublime.KindId.KEYWORD: 'kind kind_keyword',
         sublime.KindId.TYPE: 'kind kind_type',
@@ -28,73 +31,99 @@ class CodeMapManager:
         sublime.KindId.SNIPPET: 'kind kind_snippet',
     }
 
-    @staticmethod
-    def tab_name() -> str:
+    def __init__(self):
+        self._html_sheets: List[int] = []
+
+    @property
+    def tab_name(self) -> str:
         return str(settings.get("neocodemap_tab_name"))
 
-    @classmethod
-    def is_code_map(cls, sheet: Union[sublime.Sheet, sublime.HtmlSheet]) -> bool:
-        return isinstance(sheet, sublime.HtmlSheet) and sheet.id() in cls._html_sheets
+    @property
+    def layout_position(self) -> Union[Literal["left"], Literal["right"]]:
+        desired = str(settings.get("neocodemap_position")).lower()
+        if desired in ["left", "right"]:
+            return desired
 
-    @classmethod
-    def get_sheet(cls) -> Union[sublime.HtmlSheet, None]:
+        # Default to auto
+        if sublime.active_window().settings().get("sidebar_on_right"):
+            return "left"
+
+        return "right"
+
+
+    def is_code_map(self, sheet: Union[sublime.Sheet, sublime.HtmlSheet]) -> bool:
+        return isinstance(sheet, sublime.HtmlSheet) and sheet.id() in self._html_sheets
+
+    def get_sheet(self) -> Union[sublime.HtmlSheet, None]:
         for sheet in sublime.active_window().sheets():
-            if cls.is_code_map(sheet):
+            if self.is_code_map(sheet):
                 return sheet
         return None
 
-    @classmethod
-    def is_visible(cls) -> bool:
-        sheet = cls.get_sheet()
+    def is_visible(self) -> bool:
+        sheet = self.get_sheet()
         if not sheet:
             return False
         return sheet == sublime.active_window().active_sheet_in_group(
             sublime.active_window().get_sheet_index(sheet)[0]
         )
 
-    @classmethod
-    def update_sheet(cls, sheet: Optional[sublime.HtmlSheet] = None) -> bool:
+    def update_sheet(self, sheet: Optional[sublime.HtmlSheet] = None) -> bool:
         if not sheet:
-            sheet = cls.get_sheet()
+            sheet = self.get_sheet()
         if not sheet:
             return False
-        sheet.set_contents(cls.get_html())
+        sheet.set_contents(self.get_html())
 
-    @classmethod
-    def show(cls) -> bool:
-        if cls.is_visible():
+    def create_layout(self, window: sublime.Window) -> int:
+        """Create the layout and return the newly created group"""
+        layout = window.layout()
+        columns: List[float] = layout["cols"]
+        cells: List[List[int]] = layout["cells"]
+        rows: List[float] = layout["rows"]
+
+        width = float(settings.get("neocodemap_width"))
+        last_column = len(columns) - 1
+        last_row = len(rows) - 1
+
+        for i, column in enumerate(columns):
+            if column > 0:
+                columns[i] = column * (1 - width)
+
+        if self.layout_position == "right":
+            columns.append(1)
+            cells.append([last_column, 0, last_column + 1, last_row])
+
+        else:
+            columns[-1] = 1
+            columns.insert(1, width)
+            # Move everything right
+            for i, cell in enumerate(cells):
+                cells[i] = [cell[0] + 1, cell[1], cell[2] + 1, cell[3]]
+            cells.append([0, 0, 1, last_row])
+
+        window.run_command("set_layout", layout)
+        return window.num_groups()
+
+    def show(self) -> bool:
+        if self.is_visible():
             return False
 
         window = sublime.active_window()
         original_view = window.active_view()
 
         # Cleanup previous group
-        if cls.get_sheet():
-            cls.hide()
+        if self.get_sheet():
+            self.hide()
 
-        # Create new layout
-        layout = window.get_layout()
-        columns = layout["cols"]
-        cells = layout["cells"]
-        last_column = len(columns) - 1
-        last_row = len(layout["rows"]) - 1
-        width = 1 - float(settings.get("neocodemap_width"))
 
-        for i, column in enumerate(columns):
-            if column > 0:
-                columns[i] = column * width
-
-        columns.append(1)
-        cells.append([last_column, 0, last_column + 1, last_row])
-        window.run_command("set_layout", layout)
-
-        group = window.num_groups()
+        group = self.create_layout(window)
 
         # Create new sheet
         sheet = sublime.active_window().new_html_sheet(
-            cls.tab_name(), cls.get_html(original_view)
+            self.tab_name, self.get_html(original_view)
         )
-        cls._html_sheets.append(sheet.id())
+        self._html_sheets.append(sheet.id())
         window.move_sheets_to_group([sheet], group, select=False)
 
         # Restore original focus
@@ -103,9 +132,8 @@ class CodeMapManager:
 
         return True
 
-    @classmethod
-    def hide(cls) -> bool:
-        sheet = cls.get_sheet()
+    def hide(self) -> bool:
+        sheet = self.get_sheet()
         if sheet is None:
             return False
 
@@ -125,7 +153,7 @@ class CodeMapManager:
             window.run_command("close_pane", {"group": group})
 
         # Cleanup
-        cls._html_sheets.remove(sheet.id())
+        self._html_sheets.remove(sheet.id())
 
         # Restore original focus
         if original_view:
@@ -133,82 +161,24 @@ class CodeMapManager:
 
         return True
 
-    @classmethod
-    def toggle(cls) -> bool:
-        if cls.is_visible():
-            return cls.hide()
-        return cls.show()
+    def toggle(self) -> bool:
+        if self.is_visible():
+            return self.hide()
+        return self.show()
 
-    @classmethod
-    def get_html(cls, view: Optional[sublime.View] = None) -> str:
+    def get_html(self, view: Optional[sublime.View] = None) -> str:
         window = sublime.active_window()
         if not view:
             view = window.active_view()
         if not view:
             return ""
 
-        html = """
-        <style>
-        html {
-                padding: 0;
-        }
-         .kind {
-                font-weight: bold;
-                font-style: italic;
-                width: 1.5rem;
-                display: inline-block;
-                text-align: center;
-                font-family: system;
-                line-height: 1.3;
-                border-radius: 2px;
-                position: relative;
-                top: 1px;
-                margin-left: 6px;
-                margin-right: 6px;
-            }
-            .kind_ambiguous {
-                display: none;
-            }
-            .kind_keyword {
-                background-color: color(var(--pinkish) a(0.2));
-                color: var(--pinkish);
-            }
-            .kind_type {
-                background-color: color(var(--purplish) a(0.2));
-                color: var(--purplish);
-            }
-            .kind_function {
-                background-color: color(var(--redish) a(0.2));
-                color: var(--redish);
-            }
-            .kind_namespace {
-                background-color: color(var(--bluish) a(0.2));
-                color: var(--bluish);
-            }
-            .kind_navigation {
-                background-color: color(var(--yellowish) a(0.2));
-                color: var(--yellowish);
-            }
-            .kind_markup {
-                background-color: color(var(--orangish) a(0.2));
-                color: var(--orangish);
-            }
-            .kind_variable {
-                background-color: color(var(--cyanish) a(0.2));
-                color: var(--cyanish);
-            }
-            .kind_snippet {
-                background-color: color(var(--greenish) a(0.2));
-                color: var(--greenish);
-            }
-        </style>
-        <body>
-        """
+        html = f"<body><style>{css}</style>"
         for symbol in view.symbol_regions():
             kind_id, short_type, long_type = symbol.kind
             html += f"""
             <p>
-                <i class='{cls.KIND_CLASS_NAMES.get(kind_id, 'kind kind_ambiguous')}' title='{long_type}'>{short_type}</i>
+                <i class='{self.KIND_CLASS_NAMES.get(kind_id, 'kind kind_ambiguous')}' title='{long_type}'>{short_type}</i>
                 <a href='{sublime.command_url('goto_view_region_neo_code_map', {'view_id': view.id(), 'region_a': symbol.region.a, 'region_b': symbol.region.b})}'>{symbol.name}</a>
             </p>
             """
@@ -219,7 +189,7 @@ class CodeMapManager:
 
 class ToggleNeoCodeMap(sublime_plugin.TextCommand):
     def run(self, edit: sublime.Edit):
-        CodeMapManager.toggle()
+        map_manager.toggle()
 
 class GotoViewRegionNeoCodeMap(sublime_plugin.ApplicationCommand):
     def run(self, view_id: int, region_a: int, region_b: int):
@@ -234,5 +204,5 @@ class GotoViewRegionNeoCodeMap(sublime_plugin.ApplicationCommand):
 
 class SheetListener(sublime_plugin.EventListener):
     def on_modified_async(self, view: sublime.View):
-        CodeMapManager.update_sheet()
+        map_manager.update_sheet()
 
